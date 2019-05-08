7c7816780ccf505d9cad2d9510d0500a80bb0fd9
Alexander Mendelevych <sashmen5@gmail.com>
2019-05-08 10:35:17 +0300

    Counter exercise completed
    1. React redux upgrated to version 7.1.0-alpha.4 to be able to use last hooks
    
    2. Counter components updates with redux hooks

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,14 +1,23 @@
-import React, {useState} from 'react';
+import React from 'react';
+// @ts-ignore
+import { useSelector, useDispatch } from 'react-redux';
+import {decrement, increment} from "../store/actions";
+
 
 const Counter: React.FC = () => {
-  const [counter, setCounter] = useState<number>(0);
+  const counter = useSelector((state: any) => state.counter);
+  const dispatch = useDispatch();
+
+  const incrementCounter = () => dispatch(increment());
+  const decrementCounter = () => dispatch(decrement());
+
   return (
-      <div>
-          <h1>Counter</h1>
-          <div>{counter}</div>
-          <button onClick={() => setCounter(counter + 1)}>+</button>
-          <button onClick={() => setCounter(counter - 1)}>-</button>
-      </div>
+    <div>
+        <h1>Counter</h1>
+        <div>{counter}</div>
+        <button onClick={incrementCounter}>+</button>
+        <button onClick={decrementCounter}>-</button>
+    </div>
   )
 };
 
